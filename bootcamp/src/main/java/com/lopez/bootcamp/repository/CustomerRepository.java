c1e1e9a1ab62ae419684e2ae417d86d89d9e54bd
Georgia Carr <georgia.carr@autotrader.co.uk>
2019-06-12 14:18:39 +0100

    added stylistRepository

--- bootcamp/src/main/java/com/lopez/bootcamp/repository/CustomerRepository.java
+++ bootcamp/src/main/java/com/lopez/bootcamp/repository/CustomerRepository.java
@@ -1,15 +1,8 @@
 package com.lopez.bootcamp.repository;
 
-import com.lopez.bootcamp.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, String> {
-
-    @Query("FROM Customer WHERE customerEmail = :customerEmail")
-    Customer findCustomerByCustomerEmail(@Param("customerEmail") String customerEmail);
+public class CustomerRepository {
 
 }
---
+++ bootcamp/src/main/java/com/lopez/bootcamp/repository/StylistRepository.java
@@ -0,0 +1,12 @@
+package com.lopez.bootcamp.repository;
+
+import com.lopez.bootcamp.entity.Stylist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface StylistRepository extends JpaRepository<Stylist, String> {
+
+    @Query("FROM Stylist WHERE stylistEmail = :stylistEmail")
+    Stylist findStylistByStylistEmail(@Param("stylistEmail")String stylistEmail);
+}
