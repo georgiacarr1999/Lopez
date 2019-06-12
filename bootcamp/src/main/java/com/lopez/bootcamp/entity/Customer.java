200c0dfcb26616dd80f32d558fe4962620f58d5e
meriteea <anastassiameritee@rentalcars.com>
2019-06-12 13:52:06 +0100

    ana: Customer class

---
+++ bootcamp/src/main/java/com/lopez/bootcamp/entity/Customer.java
@@ -0,0 +1,25 @@
+package com.lopez.bootcamp.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "content")
+@EqualsAndHashCode(callSuper = false)
+public class Customer{
+
+    @EmbeddedId
+    private int customerId;
+
+    private String customerEmail;
+
+    private String customerName;
+
+    private String paymentDetails;
+
+}
