b923777ebbfede85fa81ae3109db396701165a3d
meriteea <anastassiameritee@rentalcars.com>
2019-06-12 14:18:52 +0100

    ana: customerRepo

--- bootcamp/src/main/java/com/lopez/bootcamp/repository/CustomerRepository.java
+++ bootcamp/src/main/java/com/lopez/bootcamp/repository/CustomerRepository.java
@@ -1,8 +1,15 @@
 package com.lopez.bootcamp.repository;
 
+import com.lopez.bootcamp.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CustomerRepository {
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+
+    @Query("FROM Customer WHERE customerEmail = :customerEmail")
+    Customer findCustomerByCustomerEmail(@Param("customerEmail") String customerEmail);
 
 }
