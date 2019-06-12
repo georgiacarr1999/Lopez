e167a985cecbd58aaa381b79a6a22e80a1fda809
Georgia Carr <georgia.carr@autotrader.co.uk>
2019-06-12 13:57:37 +0100

    added stylist

--- bootcamp/src/main/java/com/lopez/bootcamp/entity/Stylist.java
+++ bootcamp/src/main/java/com/lopez/bootcamp/entity/Stylist.java
@@ -7,23 +7,25 @@ import lombok.EqualsAndHashCode;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.util.Date;
 
 @Data
 @Entity
 @Table(name = "content")
 @EqualsAndHashCode(callSuper = false)
-public class Appointment {
+
+public class Stylist {
+
 
     @EmbeddedId
-    private int appointmentId;
-
-    private Date dateCreated;
-
-    private int customerId;
-
-    private boolean paymentTaken;
-
     private int serviceId;
 
+    private String stylistName;
+
+    private int price;
+
+    private int duration;
+
+    private String description;
+
+    private int stylistId;
 }
